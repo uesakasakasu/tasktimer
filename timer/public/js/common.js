1dd6a26dbc473ff8854aed6ced5dc4057342351f
uesakasakasu <uesakaryuuhei@gmail.com>
2026-06-22 13:29:08 +0900

    成果物をコミット

---
+++ timer/public/js/common.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+function over(target){
+	// 背景色をpinkに
+	target.style.backgroundColor = "pink";
+}
+function leave(target){
+	// 背景色を透明に
+	target.style.backgroundColor = "transparent";
+}
+
